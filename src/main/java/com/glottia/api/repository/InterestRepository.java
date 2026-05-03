14d83c4c69975afca8ddfbc578429424c64cc6be
Katherine <148381313+bxxnis@users.noreply.github.com>
2026-05-03 01:09:12 -0500

    Delete src/main/java/com/glottia/api/repository directory

--- src/main/java/com/glottia/api/repository/InterestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glottia.api.repository;
-
-import com.glottia.api.model.Interest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InterestRepository extends JpaRepository<Interest, Integer> {
-}
